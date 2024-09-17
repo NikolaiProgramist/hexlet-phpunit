0c955318c81f7fd33ea4128a1cbfafe22d58bfc1
Nikolai <NikolaiProgramist>
2024-09-17 12:11:40 +0300

    Add new test for reverseString function

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -7,9 +7,23 @@ use function Hexlet\Phpunit\Utils\reverseString;
 
 class UtilsTest extends TestCase
 {
+    public function getFixturePath(string $fixtureName)
+    {
+        $parts = [__DIR__, 'fixtures', $fixtureName];
+        return realpath(implode('/', $parts));
+    }
+
     public function testReverse(): void
     {
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
+    }
+
+    public function testReverseLong(): void
+    {
+        $text = file_get_contents($this->getFixturePath('longText.txt'));
+        $textExpected = file_get_contents($this->getFixturePath('longTextExpected.txt'));
+
+        $this->assertEquals($textExpected, reverseString($text));
     }
 }
